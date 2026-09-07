113cfc7033246c2cf820838f54d71adcb361c235
Tonoy3951573 <tasaufislamt@gmail.com>
2026-09-07 15:52:12 +0600

    Client implimented

--- src/com/simulator/model/Client.java
+++ src/com/simulator/model/Client.java
@@ -1,4 +1,22 @@
 package com.simulator.model;
 
+import java.util.UUID;
+
 public class Client {
+
+    private final String name;
+    private final String  id ;
+
+    public  Client(String name){
+        this.name =  name;
+        this.id = UUID.randomUUID().toString().replace("-","");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getId(){
+        return this.id;
+    }
 }
--- src/com/simulator/ui/SimulationController.java
+++ src/com/simulator/ui/SimulationController.java
@@ -1,4 +1,16 @@
 package com.simulator.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SimulationController {
+
+    @FXML
+    private Button AddButton;
+
+    @FXML
+    private void onAddClient(){
+        System.out.println("Client added!");
+    }
+
 }
